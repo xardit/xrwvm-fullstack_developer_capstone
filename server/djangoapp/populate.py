91c6a98289892fa67409a4400909c85918587c87
Ardit Hyka <xardit@users.noreply.github.com>
2024-10-23 23:00:26 +0200

    flake8 fixes

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -13,8 +13,9 @@ def initiate():
     car_make_instances = []
     for data in car_make_data:
         car_make_instances.append(
-            CarMake.objects.create(name=data["name"], description=data["description"])
-        )
+            CarMake.objects.create(
+                name=data["name"],
+                description=data["description"]))
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
@@ -54,9 +55,12 @@ def initiate():
             "year": 2023,
             "car_make": car_make_instances[1],
         },
-        {"name": "A4", "type": "SUV", "year": 2023, "car_make": car_make_instances[2]},
-        {"name": "A5", "type": "SUV", "year": 2023, "car_make": car_make_instances[2]},
-        {"name": "A6", "type": "SUV", "year": 2023, "car_make": car_make_instances[2]},
+        {"name": "A4", "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[2]},
+        {"name": "A5", "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[2]},
+        {"name": "A6", "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[2]},
         {
             "name": "Sorrento",
             "type": "SUV",
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -104,7 +104,8 @@ def get_cars(request):
     car_models = CarModel.objects.select_related("car_make")
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name,
+                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 
@@ -155,6 +156,7 @@ def add_review(request):
         except Exception as e:
             # If any error occurs
             print(f"Exception: {e}")
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse(
+                {"status": 401, "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
